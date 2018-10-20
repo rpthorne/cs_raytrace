06cb377ee022c0c81639fa89858d0c5278bdcd13
Ryan Thorne <rpthorne@uwm.edu>
2018-10-20 15:52:37 -0500

    changes from unix day

--- raytracing demo/XRay.h
+++ raytracing demo/XRay.h
@@ -1,17 +1,20 @@
 #pragma once
 #include "Pointf.h"
 
-
+//length assumed to be one
 class XRay
 {
 private:
 	Pointf src;
 	Pointf dir;
 	float intensity;
+	float length;
+	int reflect_count;
 public:
 	XRay XRay::refract_through(XRay& norm, float index_of_refraction_1, float index_of_refraction_2);
 	XRay();
-	XRay(Pointf, Pointf);
+	XRay(Pointf, Pointf, float intensity);
+	XRay(Pointf, Pointf, float, int);
 	XRay XRay::scale_mul(const float mul) const;
-	float XRay::magnitude_precise();
+	float XRay::length();
 };
