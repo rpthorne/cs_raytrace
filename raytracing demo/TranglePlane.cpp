600d5ff61df005a696cb434c8f042f21f7dc05c1
Ryan Thorne <rpthorne@uwm.edu>
2018-10-20 12:04:23 -0500

    Create TranglePlane.cpp

---
+++ raytracing demo/TranglePlane.cpp
@@ -0,0 +1,13 @@
+#includes "AbstractPlane.h"
+#include "pointf.h"
+#include "XRay.h"
+
+class TranglePlane : AbstractPlane
+{
+	float ray_plane_collision(XRay &p)
+	{
+		Pointf t0 = b - a;
+		Pointf t1 = c - a;
+		Pointf t2 = -p.
+	}
+}
