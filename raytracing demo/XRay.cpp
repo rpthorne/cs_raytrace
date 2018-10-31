14e886f2317457a469963728e230d95033753ed5
rpthorne <rpthorne@uwm.edu>
2018-10-30 21:56:54 -0500

    updated XRay to support most functions it needs. still needs look at refract, reflect for fresnel

--- raytracing demo/XRay.cpp
+++ raytracing demo/XRay.cpp
@@ -1,50 +1,87 @@
 #include "XRay.h"
 #include <math.h>
 
+
+
+
 //constructors
 XRay::XRay() {
 	this->dir = Pointf();
 	this->src = Pointf();
 	this->intensity = 0;
 	this->length = -1;
-	this->reflect_count = 0;
+	this->generation = 0;
+	this->current_index_of_refraction = 1;
+	this->optical_distance = 0;
 }
 
-XRay::XRay(Pointf src, Pointf dir, float intensity) {
-	this->dir = dir;
-	this->src = src;
-	this->intensity = intensity;
+XRay::XRay(Pointf const & src_, Pointf const & dir_, const float index_of_refraction_, float const & intensity_) {
+	this->dir = dir_.normalize();
+	this->src = src_;
+	this->intensity = intensity_;
 	this->length = -1;
-	this->reflect_count = 0;
+	this->generation = 0;
+	this->current_index_of_refraction = index_of_refraction_;
+	this->optical_distance = 0;
+}
+
+XRay::XRay(Pointf const & src_, Pointf const & dir_, const float index_of_refraction_, float const intensity_, int const generation_)
+{
+	this->dir = dir_.normalize();
+	this->src = src_;
+	this->intensity = intensity_;
+	this->length = -1;
+	this->generation = generation_;
+	this->current_index_of_refraction = index_of_refraction_;
+	this->optical_distance = 0;
 }
 
 //getters
-Pointf XRay::get_dir() { return this->dir; }
-Pointf XRay::get_src() { return this->dir; }
+const Pointf XRay::get_dir() const { return this->dir; }
 
-XRay::XRay(Pointf src, Pointf dir, float intensity, int reflect_count) {
-	this->dir = dir;
-	this->src = src;
-	this->intensity = intensity;
-	this->length = -1;
-	this->reflect_count = reflect_count;
+const Pointf XRay::get_src() const { return this->dir; }
+
+float XRay::get_intensity() const { return this->intensity; }
+
+float XRay::get_optic_path_length() const {return this->optical_distance + this->length * this->current_index_of_refraction;}
+
+float XRay::get_length() const { return this->length; }
+
+//setters and creaters
+
+XRay XRay::reflect(Pointf const &norm, const int total_internal) const
+{
+	Pointf new_dir = this->dir;
+	new_dir = new_dir - (norm.scale_mul(2.0f * new_dir.dot_product(norm)));
+	XRay res;
+	if(total_internal)
+		res = XRay(this->src + this->dir.scale_mul(length), new_dir, this->current_index_of_refraction, this->intensity, generation + 1);
+	else//compute intensity from fresnel
+	{
+
+		res = XRay(this->src + this->dir.scale_mul(length), new_dir, this->current_index_of_refraction, this->intensity, generation + 1);
+	}
+	return res;
 }
 
-Pointf XRay::refract_through(Pointf& norm, float index_of_refraction_1, float index_of_refraction_2)
+XRay XRay::refract(Pointf const &norm, const float index_of_refraction) const
 {
+	//if(inbound 
+
 	//normalize this
 	Pointf p = dir.scale_mul(sqrtf(dir.dot_product(dir)));
 	Pointf result;
-	float refract_index = index_of_refraction_1 / index_of_refraction_2;
+	float refract_index = this->current_index_of_refraction / index_of_refraction;
 	float vector_dot = p.dot_product(-norm);
 	result = p.scale_mul(refract_index) + norm.scale_mul(refract_index * vector_dot - sqrt(1 - refract_index * refract_index * (1 - vector_dot * vector_dot)));
 	return result;
 }
 
-XRay XRay::scale_mul(const float mul) const
+//the only place that the data of an XRay can be mutated
+int XRay::set_length(const float dist)
 {
-	XRay res;
-	res.src = src;
-	res.dir = dir.scale_mul(mul);
-	return res;
+	if (dist < 0)
+		return -1;
+	this->length = dist;
+	return 0;
 }
--- raytracing demo/XRay.h
+++ raytracing demo/XRay.h
@@ -4,20 +4,34 @@
 //length assumed to be one
 class XRay
 {
-private:
 	Pointf src;
-	Pointf dir;
+	Pointf dir; //guaranteed to be normal
 	float intensity;
-	float length;
-	int reflect_count;
-public:
-	Pointf XRay::refract_through(Pointf& norm, float index_of_refraction_1, float index_of_refraction_2);
-	XRay();
-	XRay(Pointf, Pointf, float intensity);
+	float length; // absolute length of this ray, not optical length of this ray
+	int generation;
+	float optical_distance;
+	float current_index_of_refraction;
 
-	Pointf get_dir();
-	Pointf get_src();
-	XRay(Pointf, Pointf, float, int);
-	XRay XRay::scale_mul(const float mul) const;
-	float XRay::get_length() { return length; };
+	//secret constructor for generational tracking
+	XRay(Pointf const &src_, Pointf const &dir_, const float index_of_refraction_, float const intensity_, int const generation_);
+
+public:
+	//constructers
+
+	XRay();
+	XRay(Pointf const &src_, Pointf const &dir_, const float index_of_refraction_, float const &intensity_);
+
+	//getters
+
+	const Pointf get_dir() const;
+	const Pointf get_src() const;
+	float get_intensity() const;
+	float get_optic_path_length() const;
+	float get_length() const;
+	
+	//operations
+
+	int XRay::set_length(const float dist);
+	XRay XRay::reflect(Pointf const &norm, const int total_internal) const;
+	XRay XRay::refract(Pointf const &norm, const float index_of_refraction) const;
 };
