1e31e92f3d74e64f7fd1335e7f09adf4009c48c8
rpthorne <rpthorne@uwm.edu>
2018-10-30 22:40:54 -0500

    added consts, reformated .h file

--- raytracing demo/AbstractPlane.cpp
+++ raytracing demo/AbstractPlane.cpp
@@ -1,13 +1,13 @@
 #include "AbstractPlane.h"
 #include "Pointf.h"
 
-Pointf AbstractPlane::get_normal()
+Pointf AbstractPlane::get_normal() const
 {
 	return normal;
 }
 
 
-float AbstractPlane::get_determinant(Pointf &a, Pointf &b, Pointf &c)
+float AbstractPlane::get_determinant(const Pointf &a, const Pointf &b, const Pointf &c) const
 {
 	return a.getX() * b.getY() * c.getZ() + a.getZ() * b.getX() * c.getY() + a.getY() * b.getZ() * c.getX() - \
 		a.getX() * b.getZ() * c.getY() - a.getY() * b.getX() * c.getZ() - a.getZ() * b.getY() * c.getX();
--- raytracing demo/AbstractPlane.h
+++ raytracing demo/AbstractPlane.h
@@ -5,23 +5,33 @@
 
 class AbstractPlane
 {
-public:
-	AbstractPlane(Pointf a, Pointf b, Pointf c) {
-		this->a = a;
-		this->b = b;
-		this->c = c;
-		//compute normal now, so dont have to latre
-		normal = (b - a).cross_product(c - a);
-		//normalize
-		normal = normal.scale_mul(1.0f / sqrtf(normal.dot_product(normal)));
-	}
-	//returns the amount by which p should be multiplied to the collision point
-	//only returns values for which it collides with <| p
-	virtual float ray_plane_collision(XRay &p) = 0;
-	Pointf get_normal();
 protected:
-	float get_determinant(Pointf&, Pointf&, Pointf&);
+	//to move????? not good oop
+	float get_determinant(const Pointf&, const Pointf&, const Pointf&) const;
+
+	//these represent three points that define the plane in 3d space
 	Pointf a, b, c;
 	//stored for ease of computation
 	Pointf normal;
+public:
+	AbstractPlane(const Pointf &a, const Pointf &b, const Pointf &c) {
+		this->a = a;
+		this->b = b;
+		this->c = c;
+		//compute normal now, so dont have to later
+		normal = (b - a).cross_product(c - a);
+		//normalize
+		normal = normal.normalize();
+	}
+
+	//getters
+
+	Pointf get_normal() const;
+
+	//math ops on planes
+
+	//returns the amount by which p should be multiplied to the collision point
+	//only returns values for which it collides with <| p
+	virtual float ray_plane_collision(const XRay &p) const  = 0;
+
 };
