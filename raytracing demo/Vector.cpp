4cfb1905c25e71e4c758d092414b2c53bf3dc016
NicGCox <35973566+NicGCox@users.noreply.github.com>
2018-11-08 15:00:46 -0600

    remove unnecessary methods

--- raytracing demo/Vector.cpp
+++ raytracing demo/Vector.cpp
@@ -19,26 +19,3 @@ float Vector::getMagnitude() {
 float Vector::dotProduct(Pointf &v) { 
 	return this->x * v.x + this->y * v.y + this->z * v.z; 
 }
-
-Vector Vector::scaleDiv(const float div)
-{
-	Vector res;
-	res.x = this->x / div;
-	res.y = this->y / div;
-	res.z = this->z / div;
-	return res;
-}
-
-Vector Vector::scaleMul(const float mul)
-{
-	Vector res;
-	res.x = this->x * mul;
-	res.y = this->y * mul;
-	res.z = this->z * mul;
-	return res;
-}
-
-Vector Vector::normalize()
-{
-	return scaleDiv(getMagnitude());
-}
