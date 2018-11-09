eab8d0fa072b5a5affae2a0fb10e4d81a570dc69
NicGCox <35973566+NicGCox@users.noreply.github.com>
2018-11-09 11:42:01 -0600

    operator- (with point) method

--- raytracing demo/Point.cpp
+++ raytracing demo/Point.cpp
@@ -7,3 +7,12 @@ Point::Point() : AbstractPoint(float, float, float)
 Point::Point(const float x, const float y, const float z) : AbstractPoint(x, y, z)
 {
 }
+
+Point Point::operator- (Point &c)
+{
+	Point res;
+	res.x = this->x - c.x;
+	res.y = this->y - c.y;
+	res.z = this->z - c.z;
+	return res;
+}
