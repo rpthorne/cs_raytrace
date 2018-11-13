1534411ada16ca75f18b84bae9fc535b636d3913
rpthorne <rpthorne@uwm.edu>
2018-11-13 15:13:12 -0600

    Raygun class mostly done, allows for multicamming to create ray partitions

--- raytracing demo/Raygun.h
+++ raytracing demo/Raygun.h
@@ -7,12 +7,27 @@ class Raygun
 {
 	Point x_source;
 	Point c_source;
-	float fov;
-	float aspect_ratio;
+	int ray_width;
+	int ray_height;
+	int camera_width;
+	int camera_height;
+	float w_dist;
+	float h_dist;
+	int ray_per_camera_h;
+	int ray_per_camera_w;
+	float intensity;
+	float index_of_refraction;
 public:
 	Raygun();
 	~Raygun();
 	// standard constructor
-	Raygun(const Point x_source_, const Point c_source_, float fov_, float aspect_rario_, int width_, int height_);
-	std::forward_list<XRay> create_rays();
+	Raygun(const Point x_source_, const Vector c_source_, 
+		float fov_, float aspect_ratio_, 
+		float index_of_refraction_, float intensity_, 
+		int ray_width_, int ray_height_, int camera_width_, int camera_height_);
+	Raygun(const Point x_source_, const Vector c_source_,
+		float fov_, float aspect_ratio_,
+		float index_of_refraction_, float intensity_,
+		int ray_width_, int ray_height_);
+	std::forward_list<XRay> create_rays(int camera_id);
 };
