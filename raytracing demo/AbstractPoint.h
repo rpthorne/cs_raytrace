da015872cbc33fc4f9e4a9a39c45be9dd80822fa
Nic <ngcox@uwm.edu>
2018-11-27 17:45:42 -0600

    AbstractPoint header comments

--- raytracing demo/AbstractPoint.h
+++ raytracing demo/AbstractPoint.h
@@ -1,3 +1,15 @@
+/****************************************************************
+ *	Abstract Point
+ *
+ *	Header file for the class AbstractPoint. AbstractPoint is 
+ * an object denoted by 3 floats representing the x,y, and z
+ * coordinates in three dimensional space of a point. Any class
+ * that inherits AbstractPoint will have these three protected 
+ * float values, getters for each of these, and a constructor 
+ * which takes three float values as parameters.
+ *
+ ****************************************************************/
+
 #pragma once
 
 class AbstractPoint
