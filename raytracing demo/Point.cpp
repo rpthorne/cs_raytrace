ef7bd7b47b4baa553f83f3cb71d7e5d084d2d747
Nic <ngcox@uwm.edu>
2018-11-29 18:12:02 -0600

    point documentation update

--- raytracing demo/Point.cpp
+++ raytracing demo/Point.cpp
@@ -4,7 +4,8 @@
  *	Point class inherits the AbstractPoint class. It contains a
  * default constructor, standard constructor requiring 3 floats,
  * as well as some overriding operator methods: Binary + and - as
- * well as unary -.
+ * well as unary -. Also contains a method to return the magnitude
+ * of the point as if it were a normalized vector.
  *
  ****************************************************************/
 
