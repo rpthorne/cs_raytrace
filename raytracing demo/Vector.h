46cb1bf8dfe9e4243280353c2f2b0c9d3e2fba3f
rpthorne <rpthorne@uwm.edu>
2018-12-06 21:14:55 -0600

    author, info documentation

--- raytracing demo/Vector.h
+++ raytracing demo/Vector.h
@@ -1,15 +1,13 @@
 /****************************************************************
- *	className
+ *	Vector
  *
- *	description of class
+ *	A vector represents a 3-dimensional point that has a distance
+ * to the origin of exactly 1. in other words, it is normalized.
  *
+ * @author
+ * Ryan Thorne
+ * Nick Cox
  ****************************************************************/
-
-/****************************************************************
-* Vector is a class representing a normalized vector, in that the 
-* 3 points represent directional magnitude on each axis from the 
-* origin
-*****************************************************************/
 
 #pragma once
 #include "AbstractPoint.h"
@@ -20,14 +18,20 @@ class Vector :
 {
 public:
 	
+	//an invalid vector, it points in no direction and has length 0
 	Vector();
+	//for both constructors, the represented point will be normalized
 	Vector(const float x, const float y, const float z);
 	Vector(Point p);
 
-	float get_magnitude();
+	float get_magnitude(); // always 1
 	float dot_product(Vector const &v) const;
-	Vector operator- () const;
 	Vector cross_product(Vector const &v) const;
+	
+	//inverts the direction of the vector
+	Vector operator- () const; 
+
+	//finds a point in 3-d space that represents this vector with magnitude d
 	Point traverse(float d) const;
 };
 
