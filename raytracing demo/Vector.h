ea358dcd3f0cf09f2b983805a8b8b2355a106a3d
Nic <ngcox@uwm.edu>
2018-12-06 21:02:24 -0600

    v small doc updates

--- raytracing demo/Vector.h
+++ raytracing demo/Vector.h
@@ -1,11 +1,4 @@
 /****************************************************************
- *	className
- *
- *	description of class
- *
- ****************************************************************/
-
-/****************************************************************
 * Vector is a class representing a normalized vector, in that the 
 * 3 points represent directional magnitude on each axis from the 
 * origin
