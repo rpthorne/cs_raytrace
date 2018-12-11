03153b17959e749b6b1625084632fc7de48792c2
rpthorne <rpthorne@uwm.edu>
2018-12-10 19:01:38 -0600

    inverted determinant calculation

--- raytracing demo/RectPlane.cpp
+++ raytracing demo/RectPlane.cpp
@@ -37,13 +37,13 @@ float RectPlane::ray_plane_collision(const XRay &p) const
 	//free variable(s), cannot identify single solution
 	if (fabsf(det0) < ZERO_MAX) return -1;
 
-	float detr3 = det0 / get_determinant(t0, t1, t3);// length of ray to return;
+	float detr3 = get_determinant(t0, t1, t3) / det0;// length of ray to return;
 	if (detr3 < 0) return -1;
 
-	float detr1 = det0 / get_determinant(t3, t1, t2);
+	float detr1 = get_determinant(t3, t1, t2) / det0;
 	if (detr1 < 0) return -1;
 
-	float detr2 = det0 / get_determinant(t0, t3, t2);
+	float detr2 = get_determinant(t0, t3, t2) / det0;
 	if (detr2 < 0) return -1;
 
 	if (detr2 > 1 || detr1 > 1) return -1;
